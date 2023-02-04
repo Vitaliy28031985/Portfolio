be119c802041ae1d8f93ba6bdb8dcae0690bcf95
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2023-02-04 14:05:20 +0200

    contact

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,20 @@ return (
 <div className={s.conteiner}>
 <p>Архітектор Андрій Гайдучик</p>
 <Social/>
-    <ul>
+    <ul className={s.contactConteiner}>
         <li className={s.list}>
             <a href="tel:+380968369978" className={s.link}>
                 <div className={s.conteinerItem}>
                 <p className={s.title}>Teл:</p>
                 <p>+38 (096) 837 99 78</p>
+                </div>
+            </a>
+        </li>
+        <li className={s.list}>
+            <a href="mailto:andrew.gaiduchyk@gmail.com" className={s.link}>
+                <div className={s.conteinerItem}>
+                <p className={s.title}>Email:</p>
+                <p>andrew.gaiduchyk@gmail.com</p>
                 </div>
             </a>
         </li>
