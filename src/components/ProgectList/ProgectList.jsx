9b4f84ff78d6110fde9c2ee334ef0e6163d3897f
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2023-02-05 17:45:53 +0200

    login start

--- src/components/ProgectList/ProgectList.jsx
+++ src/components/ProgectList/ProgectList.jsx
@@ -5,22 +5,55 @@ import {db} from "../../db/db";
 import {ProgektListEL} from "../ProgectListEL/ProgectListEL";
 import {ProgectModal} from "../ProgectModal/ProgectModal";
 
+const rightPassword = "12xvkhfd#95ertfs";
+
 export const ProgectList = () => {
   const [togle, setTogle] = useState(false);
+  const [showLogin, setshowLogin] = useState(false);
   const [progect, setProgect] = useState({});
+  const [password, setPassword] = useState('');
+  const [auditPassword, setAuditPassword] = useState(false);
 
-  const handleTogle = () => {
+const hedelLogin = () => {
+  setshowLogin(showLogin => !showLogin);
+}
+
+const handleTogle = () => {
     setTogle(togle => !togle);
 };
+
 const showModal = (id) => {
 const dataId = db.filter(progect => progect.id === id);
 setProgect(dataId);
 handleTogle();
 }
 
+const changePassword = (e) => {
+  const {value} = e.currentTarget;
+  setPassword(value);
+
+}
+
+
+const submitPassword = (e) => {
+  e.preventDefault();
+  if (password === '') {return alert('Введіть будь ласка пароль');}
+  if (password === rightPassword) {
+    setAuditPassword(true);
+    hedelLogin();
+    return alert('Вітаємо Вас Андрію! Ви успішно залогінились!');
+  }
+  setAuditPassword(false);
+  hedelLogin();
+  setPassword('');
+  return alert('Ви ввели неправельний пароль');
+
+} 
+
     return (
       <div>
         {togle && <ProgectModal onModal={handleTogle} progect={progect}/>}
+        <button onClick={hedelLogin} className={s.button}>Увійти</button>
         <h2 className={s.title}>Реалізовані Проекти</h2>
         <div className={s.conteiner}>
           
@@ -36,6 +69,20 @@ handleTogle();
           )
           )}
         </div>
+        {showLogin && (
+        <div className={s.loginConteiner}>
+        <form action="">
+          <label className={s.loginLabel}>Пароль</label>
+          
+          <input value={password} onChange={changePassword}/>
+    
+          <button onClick={submitPassword} className={s.loginButton}>Увійти</button>
+        </form>
+        </div>
+        )}
+   {auditPassword && (
+    <div>Hi</div>
+   )}
         </div>
     )
 }
