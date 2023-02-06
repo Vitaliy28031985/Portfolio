1536370b976e67957e9cd77d8b985cc53fa7f653
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2023-02-06 21:49:43 +0200

    21.49

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,22 @@ return (
 <p className={s.title}>Архітектор Андрій Гайдучик</p>
 <Social/>
     <ul className={s.contactConteiner}>
-        {/* <li className={s.list}>
+        <li className={s.list}>
             <a href="tel:+380968369978" className={s.link}>
                 <div className={s.conteinerItem}>
-                <p className={s.title}>Teл:</p>
+                <p className={s.titleLink}>Teл:</p>
                 <p>+38 (096) 837 99 78</p>
                 </div>
             </a>
-        </li> */}
+        </li>
+  <li className={s.list}>
+            <a href="mailto:andrew.gaiduchyk@gmail.com" className={s.link}>
+                <div className={s.conteinerItem}>
+                <p className={s.titleLink}>Email:</p>
+                <p>andrew.gaiduchyk@gmail.com</p>
+                </div>
+            </a>
+        </li>
 
         <li className={s.list}>
             <a href="tel:+380968369978" className={s.link}>
@@ -28,17 +36,7 @@ return (
                 </div>
             </a>
         </li>
-
-
-
-        {/* <li className={s.list}>
-            <a href="mailto:andrew.gaiduchyk@gmail.com" className={s.link}>
-                <div className={s.conteinerItem}>
-                <p className={s.title}>Email:</p>
-                <p>andrew.gaiduchyk@gmail.com</p>
-                </div>
-            </a>
-        </li> */}
+      
     </ul>
 </div>
 )
